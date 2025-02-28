01e7728be0e78bde870a6af162a7bd345e3fdae9
sarahib37 <129744358+sarahib37@users.noreply.github.com>
2025-02-28 21:18:29 +0100

    Portfolio Sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://yourdomain.com', // Change this to your actual domain
+    generateRobotsTxt: true, // Generates both sitemap.xml & robots.txt
+  };
+  
