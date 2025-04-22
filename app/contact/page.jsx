e54604b172be3a148a898524e4568357773d2f12
sarahib37 <129744358+sarahib37@users.noreply.github.com>
2025-04-22 21:20:29 +0100

    make contact form work

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -5,7 +5,9 @@ import { Input } from "@/components/ui/input"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 import { Textarea } from "@/components/ui/textarea"
 import { motion } from "framer-motion"
+import { useState } from "react"
 import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa"
+import emailjs from "@emailjs/browser"
 
 
 const info = [
@@ -19,14 +21,59 @@ const info = [
     title: "Email",
     description: 'sarahigwe156@gmail.com'
   },
-  {
-    icon: <FaMapMarkerAlt/>,
-    title: "Address",
-    description: 'Yenagoa, Bayelsa, Nigeria'
-  },
 ]
 
 const Contact = () => {
+
+  const [formData, setFormData] = useState({firstname: '', lastname: '', email: '', phone: '', service: '', message: ''})
+  const [loading, setLoading] = useState(false)
+
+  function handleChange(e){
+    setFormData({...formData, [e.target.name]: e.target.value })
+  }
+
+  function handleServiceChange(value){
+    setFormData({...formData, service: value })
+  }
+
+  function handleSubmit(e){
+    e.preventDefault()
+    setLoading(true)
+
+    emailjs
+    .send(
+      "service_yreaupm",
+      "template_5vd6jhc",
+      {
+        firstname: formData.firstname,
+        lastname: formData.lastname,
+        email: formData.email,
+        phone: formData.phone,
+        service: formData.service,
+        message: formData.message
+      },
+      "GeId1lpSby7eQJSxN"
+    )
+    .then(
+      (result) => {
+        alert("Message sent successfully!")
+        setLoading(false)
+        setFormData({
+          firstname: "",
+          lastname: "",
+          email: "",
+          phone: "",
+          service: "",
+          message: ""
+        })
+      },
+      (error) => {
+        console.error("Email sending error:", error.text)
+        alert("Something went wrong. Please try again later.")
+      }
+    )
+  }
+
   return (
     <motion.section
       initial = {{opacity: 0}}
@@ -39,16 +86,16 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px]">
           <div className="xl:w-[54%] order-2 xl:order-none">
-            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
+            <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl" onSubmit={handleSubmit}>
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">Whether you have a project in mind, need help with website development, or simply want to connect, feel free to drop a message. I'm always open to new opportunities and collaborations!</p>
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <Input type='firstname' placeholder='Firstname' />
-                <Input type='lastname' placeholder='Lastname' />
-                <Input type='email' placeholder='Email' />
-                <Input type='phone' placeholder='Phone number' />
+                <Input type='text' name='firstname' value={formData.firstname} onChange={handleChange} placeholder='Firstname' />
+                <Input type='text' name='lastname' value={formData.lastname} onChange={handleChange} placeholder='Lastname' />
+                <Input type='email' name='email' value={formData.email} onChange={handleChange} placeholder='Email' />
+                <Input type='text' name='phone' value={formData.phone} onChange={handleChange} placeholder='Phone number' />
               </div>
-              <Select>
+              <Select onValueChange={handleServiceChange} value={formData.service}>
                 <SelectTrigger className='w-full'>
                   <SelectValue placeholder='Select a service'/>
                 </SelectTrigger>
@@ -62,8 +109,8 @@ const Contact = () => {
                   </SelectGroup>
                 </SelectContent>
               </Select>
-              <Textarea className='h-[200px]' placeholder='Type your message here.'/>
-              <Button size='md' className='max-w-40'>Send message</Button>
+              <Textarea className='h-[200px]' placeholder='Type your message here.' name='message' value={formData.message} onChange={handleChange}/>
+              <Button size='md' className='max-w-40'>{!loading ? 'Send message' : 'Loading...'}</Button>
             </form>
           </div>
 
